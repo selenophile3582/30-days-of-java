879b91a91991d6ffcda228955029b28223fa7ff9
Dhiraj Kumar <selenophile3582@gmail.com>
2026-06-15 17:13:25 +0530

    Day 46 : Add method to find max number in an Array

---
+++ Day46_method_to_find_max.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class Day46_method_to_find_max {
+
+    public static int max(int arr[]) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int maxNum = max(arr);
+        System.out.println(maxNum);
+
+        sc.close();
+    }
+}
